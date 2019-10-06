1ef1416e822d6060adaf52f617467f3588acaac7
Vladislav Kovaliov <v.v.kovaliov@gmail.com>
2019-10-07 00:43:27 +0300

    master: done challenges

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   return runApp(
@@ -15,9 +16,50 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftDiceNumber = 1;
+  int rightDiceNumber = 1;
+  int MAX_DICE_NUMBER = 6;
+
+  void _onClickFlatBtn() {
+    setState(() {
+      leftDiceNumber = Random()
+        .nextInt(MAX_DICE_NUMBER) + 1;
+      rightDiceNumber = Random()
+        .nextInt(MAX_DICE_NUMBER) + 1;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: FlatButton(
+              onPressed: () {
+                _onClickFlatBtn();
+              },
+              child: Image.asset('images/dice$leftDiceNumber.png'),
+            ),
+          ),
+          Expanded(
+            child: FlatButton(
+              onPressed: () {
+                _onClickFlatBtn();
+              },
+              child: Image.asset('images/dice$rightDiceNumber.png'),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
+
+
